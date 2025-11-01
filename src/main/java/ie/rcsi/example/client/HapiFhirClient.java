4459bbe2d1e047135a3661997aaff9744720738e
Conor H <conor.heffron@gmail.com>
2025-11-01 22:51:49 +0000

    Create HapiFhirClient.java

---
+++ src/main/java/ie/rcsi/example/client/HapiFhirClient.java
@@ -0,0 +1,29 @@
+package ie.rcsi.example.client;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import lombok.Getter;
+
+public final class HapiFhirClient {
+
+    private static final String SERVER_BASE = "http://hapi.fhir.org/baseR4";
+
+    private static IGenericClient clientInstance;
+
+    // Create a FHIR context for R4
+    @Getter
+    private static final FhirContext fhirContext = FhirContext.forR4();
+
+    public static IGenericClient getInstance() {
+        if(clientInstance == null) {
+            clientInstance = createClient();
+        }
+
+        return clientInstance;
+    }
+
+    private static IGenericClient createClient() {
+        // Connect to the public test server
+        return fhirContext.newRestfulGenericClient(SERVER_BASE);
+    }
+}
